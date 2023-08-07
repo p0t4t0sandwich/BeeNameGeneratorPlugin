88c87e70fa094aecd6d380e6ce946a39c5285885
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-08-07 17:26:00 -0600

    Started porting to sponge8

---
+++ sponge8/src/main/java/dev/neuralnexus/beenamegenerator/sponge/SpongeBNGPlugin.java
@@ -0,0 +1,79 @@
+package dev.neuralnexus.beenamegenerator.sponge;
+
+import dev.neuralnexus.beenamegenerator.common.BeeNameGeneratorPlugin;
+import dev.neuralnexus.beenamegenerator.sponge.commands.SpongeBNGCommand;
+import dev.neuralnexus.taterlib.common.abstractions.logger.AbstractLogger;
+import dev.neuralnexus.taterlib.sponge.TemplateSpongePlugin;
+import dev.neuralnexus.taterlib.sponge.abstractions.logger.SpongeLogger;
+import org.spongepowered.api.Server;
+import org.spongepowered.api.command.Command;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
+import org.spongepowered.api.event.lifecycle.StartingEngineEvent;
+import org.spongepowered.api.event.lifecycle.StoppingEngineEvent;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.builtin.jvm.Plugin;
+import com.google.inject.Inject;
+import org.apache.logging.log4j.Logger;
+
+@Plugin("beenamegenerator")
+public class SpongeBNGPlugin extends TemplateSpongePlugin implements BeeNameGeneratorPlugin {
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private PluginContainer container;
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public AbstractLogger pluginLogger() {
+        return new SpongeLogger(logger);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void registerHooks() {}
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void registerEventListeners() {}
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void registerCommands() {}
+
+    /**
+     * Register commands.
+     * @param event The event
+     */
+    @Listener
+    public void onRegisterCommands(final RegisterCommandEvent<Command.Parameterized> event) {
+        new SpongeBNGCommand().onRegisterCommands(container, event);
+    }
+
+    /**
+     * Fired when the server starts.
+     * @param event The event
+     */
+    @Listener
+    public void onServerStarting(StartingEngineEvent<Server> event) {
+        pluginStart();
+    }
+
+    /**
+     * Fired when the server stops.
+     * @param event The event
+     */
+    @Listener
+    public void onServerStop(StoppingEngineEvent<Server> event) {
+        pluginStop();
+    }
+}
